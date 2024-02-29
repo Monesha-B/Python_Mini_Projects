e5defffe8bacc4df3df00182e8b8cfc56a99e217
Monesha_B <160079772+Monesha-B@users.noreply.github.com>
2024-02-28 22:15:08 -0500

    Add files via upload

--- Pizza_order_program.py
+++ Pizza_order_program.py
@@ -21,39 +21,39 @@ size = input("What size pizza do you want? S, M, or L: ") # What size pizza do y
 add_pepperoni = input("Do you want pepperoni? Y or N: ") # Do you want pepperoni? Y or N
 extra_cheese = input("Do you want extra cheese? Y or N ") # Do you want extra cheese? Y or N
 
-# Small_pizza_cost = 15
-# Medium_pizza_cost = 20
-# Large_pizza_cost = 25
-# Pepperoni_cost_for_small = 2
-# Pepperoni_cost_for_med_or_large = 3
-# cheese_cost = 1
+Small_pizza_cost = 15
+Medium_pizza_cost = 20
+Large_pizza_cost = 25
+Pepperoni_cost_for_small = 2
+Pepperoni_cost_for_med_or_large = 3
+cheese_cost = 1
 
-# if size == 'S' and add_pepperoni == "Y" and extra_cheese == "Y":
-#     print(f"Your final bill is: ${Small_pizza_cost+Pepperoni_cost_for_small+cheese_cost}.")
-# elif size == 'S' and add_pepperoni == "N" and extra_cheese == "Y":  
-#     print(f"Your final bill is: ${Small_pizza_cost+cheese_cost}.")
-# elif size == 'S' and add_pepperoni == "Y" and extra_cheese == "N":
-#     print(f"Your final bill is: ${Small_pizza_cost+Pepperoni_cost_for_small}.")
-# elif size == 'S' and add_pepperoni == "N" and extra_cheese == "N":
-#     print(f"Your final bill is: ${Small_pizza_cost}.")
+if size == 'S' and add_pepperoni == "Y" and extra_cheese == "Y":
+    print(f"Your final bill is: ${Small_pizza_cost+Pepperoni_cost_for_small+cheese_cost}.")
+elif size == 'S' and add_pepperoni == "N" and extra_cheese == "Y":  
+    print(f"Your final bill is: ${Small_pizza_cost+cheese_cost}.")
+elif size == 'S' and add_pepperoni == "Y" and extra_cheese == "N":
+    print(f"Your final bill is: ${Small_pizza_cost+Pepperoni_cost_for_small}.")
+elif size == 'S' and add_pepperoni == "N" and extra_cheese == "N":
+    print(f"Your final bill is: ${Small_pizza_cost}.")
 
-# if size == 'M' and add_pepperoni == "Y" and extra_cheese == "Y":
-#     print(f"Your final bill is: ${Medium_pizza_cost+Pepperoni_cost_for_med_or_large+cheese_cost}.")
-# elif size == 'M' and add_pepperoni == "N" and extra_cheese == "Y":  
-#     print(f"Your final bill is: ${Medium_pizza_cost+cheese_cost}.")
-# elif size == 'M' and add_pepperoni == "Y" and extra_cheese == "N":
-#     print(f"Your final bill is: ${Medium_pizza_cost+Pepperoni_cost_for_med_or_large}.")
-# elif size == 'M' and add_pepperoni == "N" and extra_cheese == "N":
-#     print(f"Your final bill is: ${Medium_pizza_cost}.")
+if size == 'M' and add_pepperoni == "Y" and extra_cheese == "Y":
+    print(f"Your final bill is: ${Medium_pizza_cost+Pepperoni_cost_for_med_or_large+cheese_cost}.")
+elif size == 'M' and add_pepperoni == "N" and extra_cheese == "Y":  
+    print(f"Your final bill is: ${Medium_pizza_cost+cheese_cost}.")
+elif size == 'M' and add_pepperoni == "Y" and extra_cheese == "N":
+    print(f"Your final bill is: ${Medium_pizza_cost+Pepperoni_cost_for_med_or_large}.")
+elif size == 'M' and add_pepperoni == "N" and extra_cheese == "N":
+    print(f"Your final bill is: ${Medium_pizza_cost}.")
 
-# if size == 'L' and add_pepperoni == "Y" and extra_cheese == "Y":
-#     print(f"Your final bill is: ${Large_pizza_cost+Pepperoni_cost_for_med_or_large+cheese_cost}.")
-# elif size == 'L' and add_pepperoni == "N" and extra_cheese == "Y":  
-#     print(f"Your final bill is: ${Large_pizza_cost+cheese_cost}.")
-# elif size == 'L' and add_pepperoni == "Y" and extra_cheese == "N":
-#     print(f"Your final bill is: ${Large_pizza_cost+Pepperoni_cost_for_med_or_large}.")
-# elif size == 'L' and add_pepperoni == "N" and extra_cheese == "N":
-#     print(f"Your final bill is: ${Large_pizza_cost}.")
+if size == 'L' and add_pepperoni == "Y" and extra_cheese == "Y":
+    print(f"Your final bill is: ${Large_pizza_cost+Pepperoni_cost_for_med_or_large+cheese_cost}.")
+elif size == 'L' and add_pepperoni == "N" and extra_cheese == "Y":  
+    print(f"Your final bill is: ${Large_pizza_cost+cheese_cost}.")
+elif size == 'L' and add_pepperoni == "Y" and extra_cheese == "N":
+    print(f"Your final bill is: ${Large_pizza_cost+Pepperoni_cost_for_med_or_large}.")
+elif size == 'L' and add_pepperoni == "N" and extra_cheese == "N":
+    print(f"Your final bill is: ${Large_pizza_cost}.")
 
 
 
@@ -75,7 +75,7 @@ extra_cheese = input("Do you want extra cheese? Y or N ") # Do you want extra ch
 # add_pepperoni = input()  # Do you want pepperoni? "Y" or "N"
 # extra_cheese = input()  # Do you want extra cheese? "Y" or "N"
 
-# # Your code below this line 👇
+
 # bill = 0
 # if size == "S":
 #   bill += 15
